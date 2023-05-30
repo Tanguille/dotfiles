4a64acba4ef5ef88d20afbeddc67f3e12a81dfa6
Tanguille <91473554+Tanguille@users.noreply.github.com>
2023-05-30 13:49:19 +0200

    Create config.fish

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,91 @@
+oh-my-posh init fish | source
+fastfetch
+
+# Aliases
+alias ls "/usr/bin/exa --icons --group-directories-first --color-scale -bghm"
+alias update "sudo dnf upgrade --refresh -y && sudo dnf autoremove -y && flatpak update -y && fisher update && omf update && cargo install-update -a"
+alias speedtest "python speedtest-cli"
+alias cat "bat"
+alias grep "rg"
+
+function cargo
+    if test "$argv[1]" = "install"
+        command cargo binstall $argv[2..-1]
+    else
+        command cargo $argv
+    end
+end
+
+# Env's
+## From /home/linuxbrew/.linuxbrew/bin/brew shellenv
+set -gx HOMEBREW_PREFIX     "/home/linuxbrew/.linuxbrew"
+set -gx HOMEBREW_CELLAR     "/home/linuxbrew/.linuxbrew/Cellar"
+set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
+set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths
+set -q MANPATH ; or set MANPATH  ''; set -gx MANPATH  "/home/linuxbrew/.linuxbrew/share/man"  $MANPATH
+set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
+
+# This is a hack to prevent this file from being sourced twice
+if not status is-interactive
+    exit
+end
+
+for f in ~/.config/fish/functions/private/*
+    source $f
+end
+
+# Functions
+function last_history_item; echo $history[1]; end
+abbr -a !! --position anywhere --function last_history_item
+
+# Binds option-up
+bind \e\[1\;5A history-token-search-backward
+# Binds super-up (for emacs vterm integration, where there is no "option"
+bind \e\[1\;2A history-token-search-backward
+
+# option-down
+bind \e\[1\;5B history-token-search-forward
+# super-down
+bind \e\[1\;2B history-token-search-forward
+
+# Make C-t transpose characters :)
+bind \ct transpose-chars
+
+# Make C-s accept autocompletion and submit :))
+bind \cs accept-autosuggestion execute
+
+test -e ~/.profile && source ~/.profile
+
+function postexec-source-profile --on-event fish_postexec
+    set command_line (echo $argv | string collect | string trim)
+
+    if string match -qr "^$EDITOR " "$command_line"
+        set file (echo $command_line | coln 2 | string replace '~' $HOME)
+        for config_file in ~/.profile ~/.config/fish/config.fish
+            if test (realpath -- $file) = (realpath $config_file)
+                echo -n "Sourcing "(echo $file | unexpand-home-tilde)"... "
+                source $file
+                echo done.
+            end
+        end
+    end
+end
+
+# TODO rewrite this using event emitters
+function save-error --on-event fish_postexec
+    set exit_status $status
+    set cancel_status 130
+
+    if not contains $exit_status 0 $cancel_status && \
+      not startswith retry "$argv" && \
+      not startswith sudo-retry "$argv"
+        set -g failed_command "$argv"
+    end
+end
+
+function save-edited-file --on-event fish_postexec
+    set command_line (echo $argv | string collect | string trim)
+    if string match -qr "^($EDITOR|edit) " "$command_line"
+        set -g editor_command $argv
+    end
+end
